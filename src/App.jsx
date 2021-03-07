b834627eda820f6be7c9b169eb35ebfb278aa1b2
breid <breid@activecampaign.com>
2021-03-07 06:38:09 -0800

    setup environment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './normalize.module.css';
+import './global.module.css';
+
+const App = () => {
+    return (
+        <div>I'm App</div>
+    )
+};
+
+export default App;
